2f3268fd26cb7fc018be1023fa87e3f200f29570
oblerion <oblerion@protonmail.com>
2023-06-19 17:23:22 +0200

    update

--- src/string.h
+++ src/string.h
@@ -2,16 +2,16 @@
 #define STRING_H
 #include <stdlib.h>
 #include <stdio.h>
-char* string();
-int slen(char* str);
-void saddc(char* dstr,char c);
-void scat(char* dstr,char* s);
-void sdel(char* dstr,int pos);
-void scpy(char* dstr,char* s);
-void scpyn(char* dstr,char* s,int n);
-void ssub(char* dstr,int pos);
-void ssubn(char* dstr,int pos,int n);
-int sfind(char* dstr,char* s);
-int scmp(char* dstr,char* dstr2);
-void sclear(char* dstr);
+char* string();                      //constructor 
+int slen(char* str);                 //get length
+void saddc(char* dstr,char c);       //add char to string
+void scat(char* dstr,char* s);       //cat cat s to dstr
+void sdel(char* dstr,int pos);       //delete char at pos
+void scpy(char* dstr,char* s);       //copy s to dstr
+void scpyn(char* dstr,char* s,int n);//copy n char of s to dstr
+void ssub(char* dstr,int pos);       //sub dstr begin at pos
+void ssubn(char* dstr,int pos,int n);//sub dstr begin at pos n char
+int sfind(char* dstr,char* s);       //find s to dstr
+int scmp(char* dstr,char* dstr2);    //dstr==dstr2 return 1 else return -1
+void sclear(char* dstr);             //clear to void string
 #endif
